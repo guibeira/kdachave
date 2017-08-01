953e89b04436b15793628d6aed3949930edda539
Allex Vissoci <allex.fernando@hotmail.com>
2017-08-01 20:43:58 -0300

    mascara nos campos de telefone, numero de endereco no model pessoa

--- pessoa/forms.py
+++ pessoa/forms.py
@@ -7,12 +7,12 @@ class ProprietarioForm(forms.ModelForm):
 
     class Meta:
         model = Pessoa
-        fields = ['nome','telefoneComercial', 'telefoneResidencial', 'celular1', 'celular2', 'endereco', ]
+        fields = ['nome','telefoneComercial', 'telefoneResidencial', 'celular1', 'celular2', 'endereco', 'numeroEndereco', ]
 
 class PessoaForm(forms.ModelForm):
     class Meta:
         model = Pessoa
-        fields = ['nome','telefoneComercial', 'telefoneResidencial', 'celular1', 'celular2', 'endereco', ]
+        fields = ['nome','telefoneComercial', 'telefoneResidencial', 'celular1', 'celular2', 'endereco', 'numeroEndereco', ]
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
