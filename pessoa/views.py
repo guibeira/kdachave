82c48c04324325eb13eb30829f7b411a6797a269
Doggao <gui_doggao@hotmail.com>
2017-08-01 19:09:49 -0300

    iniciando a criação do save do formulário

--- pessoa/views.py
+++ pessoa/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .forms import ProprietarioForm, PessoaForm
 # Create your views here.
 
@@ -6,15 +7,20 @@ def proprietariohome(request):
     return render(request, 'pessoa/proprietariohome.html')
 
 def proprietariocreate(request):
-    form = ProprietarioForm()
-    context = {
-        'form': form,
-        'objeto': 'Proprietario',
-    }
-
-    return render(request, 'form.html', context)
-
-
+    if request.method == 'POST':
+        form = ProprietarioForm(request.POST)
+        if form.is_valid():
+            print('formulário valido')
+        else:
+            print('deu ruim')
+        return HttpResponse(status=200)
+    else:
+        form = ProprietarioForm()
+        context = {
+            'form': form,
+            'objeto': 'Proprietario',
+        }
+        return render(request, 'form.html', context)
 
 def pessoahome(request):
     return render(request, 'pessoa/pessoahome.html')
