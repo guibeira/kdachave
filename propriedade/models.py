ba3e26b13a0b7f5447cde74b603589c97092779e
Guilherme de S. Vieira <gui_doggao@hotmail.com>
2018-03-14 21:46:00 -0300

    Ajustado modelo de endereços

--- propriedade/models.py
+++ propriedade/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from core.models import Endereco
 
-class Propriedade(models.Model):
+
+class Propriedade(Endereco):
     TIPO_PROPRIEDADE = (
                         (1, 'Apartamento'),
                         (2, 'Casa'),
@@ -9,17 +10,11 @@ class Propriedade(models.Model):
                     )
     nome = models.CharField(max_length=50)
     pessoa = models.ForeignKey('pessoa.Pessoa')
-    tipoPropriedade = models.IntegerField("Tipo Propriedade",
+    tipoPropriedade = models.IntegerField(
+                                            "Tipo Propriedade",
                                             choices=TIPO_PROPRIEDADE,
                                             default=1,
                                         )
-    cep = models.CharField("CEP",  max_length=9)
-    logradouro = models.CharField("Logradouro", max_length=150)
-    numero = models.IntegerField()
-    complemento = models.CharField("Complemento", max_length=150, null=True, blank=True)
-    bairro = models.CharField("Bairro", max_length=30)
-    cidade = models.CharField("Cidade", max_length=30)
-    estado = models.CharField("Estado", max_length=2)
 
     def __str__(self):
         return self.nome
