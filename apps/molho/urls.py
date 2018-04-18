108382858e14efd7e6c4a31713ffdbc88898d147
Guilherme de S. Vieira <gui_doggao@hotmail.com>
2018-04-18 20:47:33 -0300

    ajustado url para funcionar modal

--- apps/molho/urls.py
+++ apps/molho/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'deletar/(?P<pk>[0-9]+)/$', views.DeleteMolho.as_view(), name='delete'),
     url(r'search-molho/$', views.MolhoSearch.as_view(), name='searchmolho'),
     url(r'getmolhobypropriedade/(?P<pk>[0-9]+)/$', views.get_molho_by_propriedade, name='getmolhobypropriedade'),
-    url(r'atualizar/(?P<pk>[0-9]+)/$', views.update_molho, name='update'),
+    url(r'update/(?P<pk>[0-9]+)/$', views.update_molho, name='update'),
     url(r'updateStatus/(?P<pk>[0-9]+)/(?P<statusId>[0-9]+)/$', views.update_status, name='updateStatus'),
 
 ]
