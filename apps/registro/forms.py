34ff805ae9d3b058b93929f125f5ae035d9cb7b9
Guilherme de S. Vieira <gui_doggao@hotmail.com>
2018-04-18 22:01:06 -0300

    Adicionado filtro ao chamar formulário quando for edição

--- apps/registro/forms.py
+++ apps/registro/forms.py
@@ -1,44 +1,56 @@
 from django import forms
 from apps.registro.models import Registro
+from apps.molho.models import Molho
 
 
 class RegistroForm(forms.ModelForm):
 
-	class Meta:
-		model = Registro
-		fields = ['dataEntrega',
-				  'dataPrevisaoRetorno',
-				  'responsavel',
-				  'propriedade',
-				  'molhos'
-				  ]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        registro = kwargs.pop('instance')
+        if registro:
+            propriedade = registro.propriedade
+            choices = [(molho.pk, molho) for molho in Molho.objects.filter(propriedade=propriedade)]
+            self.fields['molhos'].choices = choices
+            self.fields['molhos'].initial = registro.molhos.all()
+
+    class Meta:
+        model = Registro
+        fields = ['dataEntrega',
+                  'dataPrevisaoRetorno',
+                  'responsavel',
+                  'propriedade',
+                  'molhos'
+                  ]
 
 
 class RegistroSaidaGetForm(forms.ModelForm):
 
-	class Meta:
-		model = Registro
-		fields = [
-				  'responsavel',
-				  'dataEntrega',
-				  'dataPrevisaoRetorno',
-				  'propriedade',
-				  ]
+    class Meta:
+        model = Registro
+        fields = [
+          'responsavel',
+          'dataEntrega',
+          'dataPrevisaoRetorno',
+          'propriedade',
+          ]
+
 
 class RegistroSaidaPostForm(forms.ModelForm):
 
-	class Meta:
-		model = Registro
-		fields = [
-				  'responsavel',
-				  'dataEntrega',
-				  'dataPrevisaoRetorno',
-				  'propriedade',
-				  'molhos',
-				  ]
+    class Meta:
+        model = Registro
+        fields = [
+          'responsavel',
+          'dataEntrega',
+          'dataPrevisaoRetorno',
+          'propriedade',
+          'molhos',
+          ]
+
 
 class RegistroDevolucaoForm(forms.ModelForm):
 
-	class Meta:
-		model = Registro
-		fields = ['molhos', 'dataRetorno']
+    class Meta:
+        model = Registro
+        fields = ['molhos', 'dataRetorno']
